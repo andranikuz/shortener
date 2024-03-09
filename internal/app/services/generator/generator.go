cb0b66e9df2612a5785ed2dc4431512f3122cfd9
Andranik Arshakyan <a.arshakian@webinar.ru>
2024-03-09 15:41:08 +0400

    iter1: Реализация http сервера

--- internal/app/services/generator/generator.go
+++ internal/app/services/generator/generator.go
@@ -2,14 +2,14 @@ package generator
 
 import (
 	"github.com/speps/go-hashids/v2"
-	"math/rand/v2"
+	"math/rand"
 )
 
 func GenerateUniqueId() string {
 	hd := hashids.NewData()
 	hashids.NewWithData(hd)
 	h, _ := hashids.NewWithData(hd)
-	e, _ := h.Encode([]int{rand.IntN(99999), rand.IntN(99999)})
+	e, _ := h.Encode([]int{rand.Int(), rand.Int()})
 
 	return e
 }
