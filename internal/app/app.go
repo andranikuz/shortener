7e268f6bb07ba3c4421f445aa0441e752950a4e8
Andranik Arshakyan <a.arshakian@webinar.ru>
2024-03-20 16:17:46 +0400

    iter6: логирование запросов

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,9 +1,13 @@
 package app
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/rs/zerolog/log"
 
 	"github.com/andranikuz/shortener/internal/config"
 	"github.com/andranikuz/shortener/internal/handlers"
@@ -25,6 +29,7 @@ func (app *Application) Init() {
 
 func (app *Application) Router() chi.Router {
 	r := chi.NewRouter()
+	r.Use(app.Logger)
 	r.Post("/", handlers.GenerateShortURLHandler)
 	r.Get("/{id}", handlers.GetFullURLHandler)
 	r.Post("/{url}", func(w http.ResponseWriter, r *http.Request) {
@@ -35,4 +40,23 @@ func (app *Application) Router() chi.Router {
 	})
 
 	return r
+}
+
+func (app *Application) Logger(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+		t1 := time.Now()
+		defer func() {
+			requestLog := fmt.Sprintf("uri=%s method=%s in %s", r.RequestURI, r.Method, time.Since(t1))
+			responseLog := fmt.Sprintf("code=%v size=%v", ww.Status(), ww.BytesWritten())
+			log.Info().
+				Str("request", requestLog).
+				Str("response", responseLog).
+				Msg("")
+		}()
+
+		next.ServeHTTP(ww, r)
+	}
+
+	return http.HandlerFunc(fn)
 }
