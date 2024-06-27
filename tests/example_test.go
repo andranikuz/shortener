da6e11ea1e193290f8632d45bfc86b7136d39f83
Andranik Arshakyan <a.arshakian@webinar.ru>
2024-06-27 13:32:41 +0400

    iter18: документация

---
+++ tests/example_test.go
@@ -0,0 +1,50 @@
+package tests
+
+import (
+	"context"
+	"fmt"
+	"github.com/andranikuz/shortener/internal/api/rest"
+	"github.com/andranikuz/shortener/internal/container"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+)
+
+func ExampleGenerateShortUrlHandler() {
+	cnt, _ := container.NewContainer()
+	h := rest.NewHTTPHandler(cnt)
+	ts := httptest.NewServer(h.Router(context.Background()))
+	reader := strings.NewReader("google.com")
+	req, _ := http.NewRequest(http.MethodPost, ts.URL, reader)
+	res, _ := ts.Client().Do(req)
+	defer res.Body.Close()
+	fmt.Println("Status Code:", res.StatusCode)
+	// Output:
+	// Status Code: 201
+}
+
+func ExampleGenerateShortUrlJSONHandler() {
+	cnt, _ := container.NewContainer()
+	h := rest.NewHTTPHandler(cnt)
+	ts := httptest.NewServer(h.Router(context.Background()))
+	reader := strings.NewReader("{\"url\": \"http://google.com\"}")
+	req, _ := http.NewRequest(http.MethodPost, ts.URL, reader)
+	res, _ := ts.Client().Do(req)
+	defer res.Body.Close()
+	fmt.Println("Status Code:", res.StatusCode)
+	// Output:
+	// Status Code: 201
+}
+
+func ExampleDeleteURLsHandler() {
+	cnt, _ := container.NewContainer()
+	h := rest.NewHTTPHandler(cnt)
+	ts := httptest.NewServer(h.Router(context.Background()))
+	reader := strings.NewReader("[\"id\"]")
+	req, _ := http.NewRequest(http.MethodDelete, ts.URL+"/api/user/urls", reader)
+	res, _ := ts.Client().Do(req)
+	defer res.Body.Close()
+	fmt.Println("Status Code:", res.StatusCode)
+	// Output:
+	// Status Code: 202
+}
