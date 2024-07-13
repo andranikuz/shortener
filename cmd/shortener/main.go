d1e122fdb40a79dda84425fdbb8ded45ada9fd22
Andranik Arshakyan <a.arshakian@webinar.ru>
2024-07-13 19:40:26 +0400

    iter20

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/rs/zerolog/log"
 
 	"github.com/andranikuz/shortener/internal/app"
+)
+
+var (
+	buildVersion = "N/A"
+	buildDate    = "N/A"
+	buildCommit  = "N/A"
 )
 
 func main() {
@@ -12,6 +20,9 @@ func main() {
 		log.Error().Msg(err.Error())
 		panic(err)
 	}
+	fmt.Printf("Build version: %s\n", buildVersion)
+	fmt.Printf("Build date: %s\n", buildDate)
+	fmt.Printf("Build commit: %s\n", buildCommit)
 	if err := a.Run(); err != nil {
 		log.Error().Msg(err.Error())
 		panic(err)
